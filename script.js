568d9950ac85663dfdeac277a3ddf410e083cd76
Avinash Kumar <avinashkumar151199@gmail.com>
2023-05-11 13:05:39 +0530

    split

---
+++ script.js
@@ -0,0 +1,9 @@
+const left= document.querySelector('.left');
+const right= document.querySelector('.right');
+const container= document.querySelector('.container');
+
+left.addEventListener('mousemove',()=>container.classList.add('hover-left'));
+left.addEventListener('mouseout',()=>container.classList.remove('hover-left'));
+
+right.addEventListener('mousemove',()=>container.classList.add('hover-right'));
+right.addEventListener('mouseout',()=>container.classList.remove('hover-right'));
